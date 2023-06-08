cfd4300505a3ab84ce90197b964389af134276a8
elsatobin <elsa0010021@outlook.com>
2023-06-08 10:58:15 +0300

    Update page.jsx

--- src/app/shop/[productid]/page.jsx
+++ src/app/shop/[productid]/page.jsx
@@ -1,18 +1,8 @@
 import axios from "axios";
 import dynamic from "next/dynamic";
 
-export async function generateMetadata({ params, searchParams }, parent) {
-	const id = params.id;
 
-	const productMeta = await axios
-		.get(`http://localhost:3000/assist/productsData/product_${id || 1}.json`)
-		.then((res) => res.data);
 
-	return {
-		title: productMeta && productMeta.title,
-		description: productMeta && productMeta.name,
-	};
-}
 
 export default function Productid() {
 	const ProductDetais = dynamic(
